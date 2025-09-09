73127e51c40bc54608d14ce7059d90abc7fc7a72
Tymivv <43436409+Tymivv@users.noreply.github.com>
2025-09-09 15:59:22 +0300

    Update goit_algo_hw_03.py

--- goit_algo_hw_03.py
+++ goit_algo_hw_03.py
@@ -3,18 +3,27 @@ import random
 
 
 def string_to_date(date_string):
-    return datetime.strptime(date_string, "%Y-%m-%d").date()
+    try:
+        return datetime.strptime(date_string, "%Y-%m-%d").date()
+    except ValueError:
+        return None   # або можна повернути повідомлення "Невірний формат"
 
-def get_days_from_today(date):
-    return (datetime.today().date() - string_to_date(date)).days
+def get_days_from_today(date_str):
+    d = string_to_date(date_str)
+    if d is None:
+        return "Неправильний формат дати. Використовуйте YYYY-MM-DD"
+    return (datetime.today().date() - d).days
 
-# print(get_days_from_today("2021-10-09"))
+# print(get_days_from_today("13.14.2000"))
 
 def get_numbers_ticket(min, max, quantity):
     cards = []
     if min <0 or max > 1000 :
-        return "Неприпустимі значення"
-    return sorted(random.sample(range(min, max + 1), quantity))
+        return cards
+    if quantity > (max - min + 1):
+        return cards
+    cards = sorted(random.sample(range(min, max + 1), quantity))
+    return cards
 
 
-# print(get_numbers_ticket(1, 49, 6))
+# print(get_numbers_ticket(1, 49000, 5))
